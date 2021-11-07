be726752b50f8d6c3aed4db1e74fb1d250fd5186
Dima Tsymbal <dima.tsymbal12345@gmail.com>
2021-11-07 16:47:33 +0200

    js

---
+++ script.js
@@ -0,0 +1,18 @@
+const submit = document.querySelector('.submit')
+
+
+submit.addEventListener('click', (e) => {
+    // e.preventDefault()
+    const input1 = document.querySelector('.input1')
+    const input2 = document.querySelector('.input2')
+
+    let out1 = document.querySelector('.out1')
+    let out2 = document.querySelector('.out2')
+    
+    out1.innerText = input1.value
+    out2.innerText = input2.value
+    
+
+    window.localStorage.setItem('username', input1.value)
+    window.localStorage.setItem('password', input2.value)
+})
